73313626505c7838c450914f3ff48f5da5ec8328
Live session user <przemyslaw.stoklosa@gmail.com>
2020-07-04 12:51:34 +0000

    cd..

--- src/main/java/com/example/java/Main.java
+++ src/main/java/com/example/java/Main.java
@@ -1,7 +1,32 @@
 package com.example.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    ArrayList<Integer> integerArray;
+    ArrayList<Double> doubleArray;
+
+    ArrayList<String> stringArray;
+
     public static void main(String[] args) {
-        System.out.println("napis");
+        Main main = new Main();
+        main.example();
+    }
+
+    void example(){
+        fillArrays();
+    }
+
+    <T,U> void printArrays(ArrayList<T> data1, ArrayList<U> data2){
+
+    }
+
+    void fillArrays(){
+        for(int i=0;i<10;i++){
+            integerArray.add(i);
+            doubleArray.add((double)i);
+            stringArray.add("text: "+((Integer)i).toString());
+        }
     }
 }
